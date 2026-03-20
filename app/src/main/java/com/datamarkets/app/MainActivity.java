bb26cdcd455dedf4978ff20d397165264b6ef268
Manuel <manuelmntarco@gmail.com>
2026-03-20 20:02:04 +0100

    Añade navegacion ingerior con BottomNavigationView y NavController

--- app/src/main/java/com/datamarkets/app/MainActivity.java
+++ app/src/main/java/com/datamarkets/app/MainActivity.java
@@ -2,6 +2,10 @@ package com.datamarkets.app;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Obtiene el NavController desde el FragmentContainerView
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+
+        // Conecta la barra inferior con el NavController
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        NavigationUI.setupWithNavController(bottomNav, navController);
     }
 }
